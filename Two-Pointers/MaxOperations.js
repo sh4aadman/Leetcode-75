7a2c8df385923d7b07afa8966f8c72b1b0489592
sh4aadman <sh4adman@gmail.com>
2026-02-13 01:31:35 +0600

    13-02-26

---
+++ Two-Pointers/MaxOperations.js
@@ -0,0 +1,18 @@
+// Leetcode - Problem 1679
+
+// function maxOperations(nums, k) {
+//     nums.sort((a, b) => a - b);
+//     var left = 0, right = nums.length - 1;
+//     while (left < right) {
+//         var sum = nums[left] + nums[right];
+
+//     }
+
+//   return count;
+// }
+
+console.log(maxOperations([1, 2, 3, 4], 5));
+console.log(maxOperations([3, 1, 3, 4, 3], 6));
+
+// Time Complexity - O(n)
+// Space Complexity - O(1)
--- Two-Pointers/MoveZeroes.js
+++ Two-Pointers/MoveZeroes.js
@@ -1,6 +1,6 @@
 // Leetcode - Problem 283
 
-// Intuition -
+// Intuition - A simple realization is if the current element is non-0, its' correct position can at best be it's current position or a position earlier. Continue iterating until all elements are processed, ensuring all non-zero elements are moved to the front of the array and zeros are pushed to the end.
 
 function moveZeroes(nums) {
   for (var write = 0, i = 0; i < nums.length; i++) {
